9ce8890bb42eab27410d983511072b76038acfbb
romain.fontugne <romain.fontugne@gmail.com>
2021-02-05 09:12:51 +0000

    add spamhaus prefix drop list

---
+++ scripts/decix_routeserver.py
@@ -0,0 +1,138 @@
+import sys
+import requests
+import json
+import wikihandy
+
+# URL to the API
+URL_CONFIG = 'https://lg.de-cix.net/api/v1/config'
+URL_RS = 'https://lg.de-cix.net/api/v1/routeservers'
+URL_NEIGHBOR = 'https://lg.de-cix.net/api/v1/routeservers/{rs}/neighbors'
+URL_ROUTE = 'https://lg.de-cix.net/api/v1/routeservers/{rs}/neighbors/{neighbor}/routes/received'
+# Name of the organization providing the data
+ORG = 'DE-CIX Management GmbH'
+
+class Crawler(object):
+    def __init__(self):
+        """Initialize wikihandy """
+    
+        # Helper for wiki access
+        self.wh = wikihandy.Wikihandy()
+
+        # Added properties will have this additional information
+        self.org_qid = self.wh.get_qid(ORG)
+
+        self.reference_config = [
+            (self.wh.get_pid('source'), self.org_qid),
+            (self.wh.get_pid('reference URL'), URL_RS),
+            (self.wh.get_pid('point in time'), today),
+            ]
+
+        self.rs_config = self.fetch( URL_CONFIG )
+        self.rs_asn_qid = self.asn2qid(self.rs_config['asn'], create=True)
+
+
+    def fetch(self, url):
+        req = requests.get( url )
+        if req.status_code != 200:
+            sys.exit('Error while fetching data')
+        return json.loads(req.text)
+
+
+    def run(self):
+        """Fetch data from API and push to wikibase. """
+
+        self.wh.login() # Login once for all threads
+
+        today = self.wh.today()
+
+        routeservers = self.fetch(URL_RS)['routeservers']
+
+        self.reference = [
+            (self.wh.get_pid('source'), self.org_qid),
+            (self.wh.get_pid('reference URL'), URL_RS),
+            (self.wh.get_pid('point in time'), today),
+            ]
+
+        for rs in routeservers:
+            # Register/update route server 
+            self.update_rs(rs)
+
+            # route server neighbors
+            self.url_neighbor = URL_NEIGHBOR.format(rs=rs['id'])
+            self.reference_neighbor = [
+                (self.wh.get_pid('source'), self.org_qid),
+                (self.wh.get_pid('managed by'), self.rs_qid),
+                (self.wh.get_pid('reference URL'), self.url_neighbor),
+                (self.wh.get_pid('point in time'), today),
+                ]
+
+            neighbors = self.fetch(url_neighbor)['neighbors']
+            for n in neighbors:
+                self.update_neighbor(n)
+
+            # 
+            self.url_route = URL_ROUTE.format(rs=rs['id'], neighbor=neighbor['id'])
+            self.reference_route = [
+                (self.wh.get_pid('source'), self.org_qid),
+                (self.wh.get_pid('reference URL'), self.url_route),
+                (self.wh.get_pid('point in time'), today),
+                ]
+
+            routes = self.fetch(url_route)
+            # Imported routes
+            for route in routes['imported']:
+                self.update_route(route,'imported')
+                sys.stderr.write(f'\rProcessing {country.name}... {i+1}/{len(selected)}')
+
+            sys.stderr.write('\n')
+
+    def update_route(self, route, status):
+        """Update route data"""
+
+        asn_qid = self.wh.asn2qid(route['bgp']['as_path'][-1], create=True) 
+        # Properties
+        statements = [ [ self.wh.get_pid('appeared in'), self.rs_qid, self.reference_route] ]
+        statements.append( [self.wh.get_pid('announced by'), asn_qid, self.reference_route]) 
+        prefix_qid = self.wh.prefix2qid(route['network'], create=True) 
+        self.wh.upsert_statements('update from route server API', prefix_qid, statements)
+
+
+    def update_neighbor(self, neighbor):
+        """Update AS neighbor data"""
+
+        # Properties
+        statements = [ [ self.wh.get_pid('external ID'), neighbor['id'], self.reference_neighbor] ]
+        asn_qid = self.wh.asn2qid(neighbor['asn'], create=True) 
+        self.wh.upsert_statements('update from route server API', asn_qid, statements)
+
+    def update_rs(self, rs):
+        """Update route server data or create if it's not already there"""
+
+        # Properties
+        statements = []
+
+        # set ASN
+        statements.append( [ self.wh.get_pid('autonomous system number'), self.rs_config['asn'] , self.reference_config])
+
+        # set org
+        statements.append( [ self.wh.get_pid('managed by'), self.org_qid, self.reference])
+
+        # set IXP (assumes the res['group'] is the same as peeringdb ix name)
+        statements.append( [ self.wh.get_pid('part of'), self.wh.get_qid(res['group']), self.reference])
+
+        # set external id
+        statements.append( [ self.wh.get_pid('external ID'), self.wh.get_qid(res['id']), self.reference])
+
+        # Commit to wikibase
+        self.rs_qid = self.wh.get_qid(f"{rs['name']}", create={  
+                'summary': 'add route server from Alice API',
+                'description': f"Route server in {rs['group']}"
+                'statements': [ [self.wh.get_pid('instance of'), self.wh.get_qid('route server')], ]
+                })
+        self.wh.upsert_statements('update from route server API', self.rs_qid, statements)
+
+        
+# Main program
+if __name__ == '__main__':
+    crawler = Crawler()
+    crawler.run()
---
+++ scripts/spamhaus_prefix_edrop.py
@@ -0,0 +1,90 @@
+import sys
+import requests
+import pybgpstream
+import wikihandy
+
+# URL to spamhaus data
+URL = 'https://www.spamhaus.org/drop/edrop.txt'
+
+class Crawler(object):
+    def __init__(self):
+        """
+        """
+    
+        # Helper for wiki access
+        self.wh = wikihandy.Wikihandy(preload=True)
+
+        # Get the QID for Spamhaus organization
+        self.spamhaus_qid = self.wh.get_qid('Spamhaus',
+            create={                                    # Create it if it doesn't exist
+                'summary': 'add Spamhaus organization',         # Commit message
+                'description': 'The Spamhaus Project is an international organisation to track email spammers and spam-related activity',    # Item description
+                'aliases': 'The Spamhaus Project|the spamhaus project',
+                'statements': [ [self.wh.get_pid('instance of'), self.wh.get_qid('organization')]]
+                })
+
+        # Get the QID for Spamhaus DROP project
+        self.drop_qid = self.wh.get_qid('Spamhaus DROP lists',
+            create={                                    # Create it if it doesn't exist
+                'summary': 'add Spamhaus block list',         # Commit message
+                'description': "The Spamhaus Don't Route Or Peer Lists",    # Item description
+                'statements': [[self.wh.get_pid('managed by'), self.spamhaus_qid]]
+                })
+
+        # Get the QID for Spamhaus EDROP list
+        self.drop_qid = self.wh.get_qid('Spamhaus EDROP list',
+            create={                                    # Create it if it doesn't exist
+                'summary': 'add Spamhaus block list',         # Commit message
+                'description': 'EDROP is an extension of the DROP list that includes suballocated netblocks controlled by spammers or cyber criminals',
+                'statements': [[self.wh.get_pid('managed by'), self.spamhaus_qid],
+                                [self.wh.get_pid('part of'), self.drop_qid]]
+                })
+
+        # Added properties will have this additional information
+        today = self.wh.today()
+        self.reference = [
+                (self.wh.get_pid('source'), self.spamhaus_qid),
+                (self.wh.get_pid('reference URL'), URL),
+                (self.wh.get_pid('point in time'), today)
+                ]
+
+
+
+    def run(self):
+        """Fetch blocklist from Spamhaus and push to wikibase. """
+
+        req = requests.get(URL)
+        if req.status_code != 200:
+            sys.exit('Error while fetching the blocklist')
+
+        for i, row in enumerate( req.text.splitlines() ):
+            # Skip the header
+            if row.startswith(';'):
+                continue
+
+            self.update_net(row)
+            sys.stderr.write(f'\rProcessed {i+1} prefixes')
+        sys.stderr.write(f'\n')
+
+
+    def update_net(self, one_line):
+        """Add the prefix to wikibase if it's not already there and update its
+        properties."""
+
+        prefix, _, _ = one_line.partition(';')
+
+        # Properties for this prefix
+        statements = [ 
+                [self.wh.get_pid('reported in'), self.drop_qid, self.reference],
+                ] 
+
+
+        # Commit to wikibase
+        # Get the prefix QID (create if prefix is not yet registered) and commit changes
+        net_qid = self.wh.prefix2qid(prefix, create=True) 
+        self.wh.upsert_statements('update from Spamhaus EDROP list', net_qid, statements )
+        
+# Main program
+if __name__ == '__main__':
+    crawler = Crawler()
+    crawler.run()
