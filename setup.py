b2227497fcb7f14dd8a60ad11bdae22072677af4
romain.fontugne <romain.fontugne@gmail.com>
2021-03-08 02:45:23 +0000

    fixed setup / package folders

--- setup.py
+++ setup.py
@@ -2,7 +2,5 @@ from setuptools import setup, find_packages
 setup(
     version = '0.0.1',
     name = 'iyp',
-    packages = find_packages(
-    ),
-    package_dir = {"":"src"}
+    packages = find_packages(),
 )
