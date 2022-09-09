6d4b91ab83ef62d9ae7f41966bf6c620f097f196
romain.fontugne <romain.fontugne@gmail.com>
2022-09-09 17:00:07 +0900

    add basic constraints and fix get_node

--- iyp/crawlers/ripe/iyp.py
+++ iyp/crawlers/ripe/iyp.py
@@ -3,6 +3,18 @@ from neo4j import GraphDatabase
 
 # TODO: get config from configuration file
 
+def dict2str(d):
+
+    data = [] 
+    for key, value in d.items():
+        if isinstance(value, str) and '"' in value:
+            data.append(f"{key}: '{value}'")
+        elif isinstance(value, str):
+            data.append(f'{key}: "{value}"')
+        else:
+            data.append(f'{key}: {value}')
+
+    return '{'+','.join(data)+'}'
 
 class IYP(object):
 
@@ -16,20 +28,68 @@ class IYP(object):
         self.password = "password"
 
         # Connect to the database
-        uri = "neo4j://{self.server}:{self.port}"
+        uri = f"neo4j://{self.server}:{self.port}"
         self.db = GraphDatabase.driver(uri, auth=(self.login, self.password))
-        self.session = self.db.session() 
+        self.session = self.db.session()
+
+        self._db_init()
+
+    def _db_init(self):
+        """Add usual constrains"""
+
+        # Constrains for ASes
+        self.session.run(
+            " CREATE CONSTRAINT AS_UNIQUE_ASN IF NOT EXISTS "
+            " FOR (n:AS) "
+            " REQUIRE n.asn IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT AS_NOTNULL_ASN IF NOT EXISTS "
+            " FOR (n:AS) "
+            " REQUIRE n.asn IS NOT NULL ")
+
+        # Constrains for prefixes
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_UNIQUE_PREFIX IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.prefix IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_NOTNULL_PREFIX IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.prefix IS NOT NULL ")
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_NOTNULL_af IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.af IS NOT NULL ")
+
+        # Constrains for IPs
+        self.session.run(
+            " CREATE CONSTRAINT IP_UNIQUE_IP IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.ip IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT IP_NOTNULL_IP IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.ip IS NOT NULL ")
+        self.session.run(
+            " CREATE CONSTRAINT IP_NOTNULL_IP IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.af IS NOT NULL ")
+
+
 
     def get_node(self, type, prop, create=False):
         """Find the ID of a node in the graph. Return None if the node does not
         exist or create the node if create=True."""
 
         if create:
-            result = self.session.run("MERGE (a:$type $prop) RETURN ID(a)", type=type, prop=prop).single()
+            result = self.session.run(f"MERGE (a:{type} {dict2str(prop)}) RETURN ID(a)").single()
         else:
-            result = self.session.run("MATCH (a:$type $prop) RETURN ID(a)", type=type, prop=prop).single()
+            result = self.session.run(f"MATCH (a:{type} {dict2str(prop)}) RETURN ID(a)").single()
 
-        return result
+        if result is not None:
+            return result[0]
+        else:
+            return None
 
 
     def add_links(self, src_node, links):
@@ -37,10 +97,10 @@ class IYP(object):
         links. This parameter is a list of [link_type, dst_node_id, prop_dict]."""
 
         for type, dst_node, prop in links:
-            self.session.run("MATCH (a) WHERE ID(a) = $src_node "
-                "CREATE (a)-[:$type]->(b $prop) WHERE ID(b) = $dst_node",
-                src_node=src_node, type=type, prop=prop)
-
+            self.session.run(
+                f"MERGE (a)-[:{type}]->(b {prop})" 
+                "WHERE ID(a) = $src_node ID(b) = $dst_node",
+                src_node=src_node, dst_node=dst_node)
 
     def close(self):
         self.session.close()
