63618270f06d7ca8ce65c87333bba34685d6a2e0
romain.fontugne <romain.fontugne@gmail.com>
2022-09-13 23:28:09 +0900

    Fix property formating for common properties (ASN, IPs, etc..)

---
+++ iyp/__init__.py
@@ -0,0 +1,162 @@
+import logging
+import datetime
+from neo4j import GraphDatabase
+
+
+def format_properties(prop):
+    """Make sure certain properties are always formatted the same way.
+    For example IPv6 addresses are stored in lowercase, or ASN are kept as 
+    integer not string."""
+
+    # asn is stored as an int
+    if 'asn' in prop:
+        prop['asn'] = int(prop['asn'])
+
+    # ipv6 is stored in lowercase
+    if 'ip' in prop:
+        prop['ip'] = prop['ip'].lower()
+    if 'prefix' in prop:
+        prop['prefix'] = prop['prefix'].lower()
+
+    # country code is kept in capital letter
+    if 'country_code' in prop:
+        prop['country_code'] = prop['country_code'].upper()
+
+    return prop
+
+
+def dict2str(d):
+    """Converts a python dictionary to a Cypher map."""
+
+    data = [] 
+    for key, value in d.items():
+        if isinstance(value, str) and '"' in value:
+            escaped = value.replace("'", r"\'")
+            data.append(f"{key}: '{escaped}'")
+        elif isinstance(value, str) or isinstance(value, datetime.datetime):
+            data.append(f'{key}: "{value}"')
+        else:
+            data.append(f'{key}: {value}')
+
+    return '{'+','.join(data)+'}'
+
+
+class IYP(object):
+
+    def __init__(self):
+
+        logging.debug('Wikihandy: Enter initialization')
+
+        # TODO: get config from configuration file
+        self.server = 'localhost'
+        self.port = 7687
+        self.login = "neo4j"
+        self.password = "password"
+
+        # Connect to the database
+        uri = f"neo4j://{self.server}:{self.port}"
+        self.db = GraphDatabase.driver(uri, auth=(self.login, self.password))
+        self.session = self.db.session()
+
+        self._db_init()
+
+    def _db_init(self):
+        """Add usual constrains"""
+
+        # Constrains for ASes
+        self.session.run(
+            " CREATE CONSTRAINT AS_UNIQUE_ASN IF NOT EXISTS "
+            " FOR (n:AS) "
+            " REQUIRE n.asn IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT AS_NOTNULL_ASN IF NOT EXISTS "
+            " FOR (n:AS) "
+            " REQUIRE n.asn IS NOT NULL ")
+
+        # Constrains for prefixes
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_UNIQUE_PREFIX IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.prefix IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_NOTNULL_PREFIX IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.prefix IS NOT NULL ")
+        self.session.run(
+            " CREATE CONSTRAINT PREFIX_NOTNULL_AF IF NOT EXISTS "
+            " FOR (n:PREFIX) "
+            " REQUIRE n.af IS NOT NULL ")
+
+        # Constrains for IPs
+        self.session.run(
+            " CREATE CONSTRAINT IP_UNIQUE_IP IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.ip IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT IP_NOTNULL_IP IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.ip IS NOT NULL ")
+        self.session.run(
+            " CREATE CONSTRAINT IP_NOTNULL_AF IF NOT EXISTS "
+            " FOR (n:IP) "
+            " REQUIRE n.af IS NOT NULL ")
+
+        # Constrains for countries
+        self.session.run(
+            " CREATE CONSTRAINT COUNTRY_UNIQUE_CC IF NOT EXISTS "
+            " FOR (n:COUNTRY) "
+            " REQUIRE n.country_code IS UNIQUE ")
+        self.session.run(
+            " CREATE CONSTRAINT COUNTRY_NOTNULL_CC IF NOT EXISTS "
+            " FOR (n:COUNTRY) "
+            " REQUIRE n.country_code IS NOT NULL ")
+
+
+    def get_node(self, type, prop, create=False):
+        """Find the ID of a node in the graph. Return None if the node does not
+        exist or create the node if create=True."""
+
+        prop = format_properties(prop)
+
+        if create:
+            result = self.session.run(f"MERGE (a:{type} {dict2str(prop)}) RETURN ID(a)").single()
+        else:
+            result = self.session.run(f"MATCH (a:{type} {dict2str(prop)}) RETURN ID(a)").single()
+
+        if result is not None:
+            return result[0]
+        else:
+            return None
+
+
+    def add_links(self, src_node, links):
+        """Create links from src_node to the destination nodes given in parameter
+        links. This parameter is a list of [link_type, dst_node_id, prop_dict].
+        The dictionary prop_dict should at least contain a 'source', 'point in time', 
+        and 'reference URL'. Keys in this dictionary should contain no space.
+
+        By convention link_type is written in UPPERCASE and keys in prop_dict are
+        in lowercase."""
+
+        
+        for type, dst_node, prop in links:
+
+            assert 'source' in prop
+            assert 'reference_url' in prop
+            assert 'point_in_time' in prop
+
+            prop = format_properties(prop)
+
+            self.session.run(
+                " MATCH (a), (b) " 
+                " WHERE ID(a) = $src_node AND ID(b) = $dst_node "
+                f" MERGE (a)-[:{type}  {dict2str(prop)}]->(b) ",
+                src_node=src_node, dst_node=dst_node).consume()
+
+
+    def close(self):
+        self.session.close()
+        self.db.close()
+
+
+
--- iyp/crawlers/ripe/as_names_neo4j.py
+++ iyp/crawlers/ripe/as_names_neo4j.py
@@ -3,23 +3,25 @@ import logging
 import requests
 import datetime
 from iyp import IYP
+# TODO move iyp to a better place
 #from ...lib.wikihandy import Wikihandy
 
 URL_RIPE_AS_NAME = 'https://ftp.ripe.net/ripe/asnames/asn.txt'
 
 class Crawler(object):
     def __init__(self):
-        # connection to neo4j
-
-        # Helper for wiki access
-        #self.wh = Wikihandy()
 
         # Reference information for data pushed to the wikibase
-        self.reference = [
-            ('source', 'RIPE NCC'),
-            ('reference URL', URL_RIPE_AS_NAME),
-            ('point in time', datetime.datetime.utcnow())
-            ]
+        self.reference = {
+            'source': 'RIPE NCC',
+            'reference_url': URL_RIPE_AS_NAME,
+            'point_in_time': datetime.datetime.utcnow()
+                .replace(hour=0, minute=0, second=0, microsecond=0)
+            }
+
+        # connection to IYP database
+        self.iyp = IYP()
+
 
     def run(self):
         """Fetch the AS name file from RIPE website and process lines one by one"""
@@ -28,29 +30,29 @@ class Crawler(object):
         if req.status_code != 200:
             sys.exit('Error while fetching AS names')
 
-        # Start neo4j session
-        with self.iyp.session() as session:
-        
-            for i, _ in enumerate(map(self.update_asn, req.text.splitlines())):
-                sys.stderr.write(f'\rProcessed {i} ASes')
-            
+        for i, _ in enumerate(map(self.update_asn, req.text.splitlines())):
+            sys.stderr.write(f'\rProcessed {i} ASes')
+
+        sys.stderr.write('\n')
+        self.iyp.close()
 
     def update_asn(self, one_line):
         # Parse given line to get ASN, name, and country code 
         asn, _, name_cc = one_line.partition(' ')
         name, _, cc = name_cc.rpartition(', ')
 
-        asn_qid = self.iyp.get_asn(asn, create=True)
-        cc_qid = self.iyp.get_country(cc, create=True)
+        asn_qid = self.iyp.get_node('AS', {'asn': asn}, create=True)
+        cc_qid = self.iyp.get_node('COUNTRY', {'country_code': cc}, create=True)
+        name_qid = self.iyp.get_node('NAME', {'name': name}, create=True)
 
         statements = []
-        statements.append( [self.wh.get_pid('country'), cc_qid, self.reference] )  # Set country
+        statements.append( ['COUNTRY', cc_qid, self.reference] )  # Set country
         if cc_qid is not None:
-            statements.append( [self.wh.get_pid('name'), name, self.reference] )       # Set AS name
+            statements.append( ['NAME', name_qid, self.reference] ) # Set AS name
 
         try:
             # Update AS name and country
-            self.wh.upsert_statements('updates from RIPE AS names', asn_qid, statements)
+            self.iyp.add_links(asn_qid, statements)
 
         except Exception as error:
             # print errors and continue running
