397c3b3c8d47f2a2982b61df67e77e106187f199
romain.fontugne <romain.fontugne@gmail.com>
2022-11-08 23:05:54 +0900

    add apnic and caida rankings in neo4j

--- iyp/crawlers/caida/asrank.py
+++ iyp/crawlers/caida/asrank.py
@@ -2,42 +2,30 @@ import sys
 import logging
 import requests
 import json
-from concurrent.futures import ThreadPoolExecutor
-from iyp.wiki.wikihandy import Wikihandy
+from datetime import datetime, time
+from iyp import IYP
 
 # URL to ASRank API
 URL_API = 'https://api.asrank.caida.org/v2/restful/asns/'
 
 class Crawler(object):
     def __init__(self):
-        """Initialize wikihandy and qualifiers for pushed data"""
+        """Initialize iyp connection and qualifiers for pushed data"""
     
-        # Helper for wiki access
-        self.wh = Wikihandy()
+        self.reference = {
+            'source': 'CAIDA',
+            'reference_url': URL_API,
+            'point_in_time': datetime.combine(datetime.utcnow(), time.min)
+            }
 
-        # Added properties will have this additional information
-        today = self.wh.today()
-        self.caida_qid = self.wh.get_qid('CAIDA')
-
-        # Get the QID for ASRank project
-        self.asrank_qid = self.wh.get_qid('CAIDA ASRank',
-            create={                                    # Create it if it doesn't exist
-                'summary': 'add CAIDA ASRank',         # Commit message
-                'description': "CAIDA's AS ranking derived from topological data collected by CAIDA's Archipelago Measurement Infrastructure and BGP routing data collected by the Route Views Project and RIPE NCC.", # Item description
-                'statements': [[self.wh.get_pid('managed by'), self.caida_qid]]
-                })
-
-        self.reference = [
-                (self.wh.get_pid('source'), self.caida_qid),
-                (self.wh.get_pid('reference URL'), URL_API),
-                (self.wh.get_pid('point in time'), today)
-                ]
+        # connection to IYP database
+        self.iyp = IYP()
 
     def run(self):
         """Fetch networks information from ASRank and push to wikibase. """
 
-        self.wh.login() # Login once for all threads
-        pool = ThreadPoolExecutor()
+        self.asrank_qid = self.iyp.get_node('RANKING', {'name': f'CAIDA ASRank'}, create=True)
+
         has_next = True
         i = 0
         while has_next:
@@ -48,14 +36,13 @@ class Crawler(object):
             ranking = json.loads(req.text)['data']['asns']
             has_next = ranking['pageInfo']['hasNextPage']
 
-            for res in pool.map(self.update_net, ranking['edges']):
+            for _ in map(self.update_net, ranking['edges']):
                 sys.stderr.write(f'\rProcessing... {i+1}/{ranking["totalCount"]}')
                 i+=1
 
-        pool.shutdown()
 
     def update_net(self, asn):
-        """Add the network to wikibase if it's not already there and update its
+        """Add the network to iyp if it's not already there and update its
         properties."""
         
         asn = asn['node']
@@ -64,24 +51,25 @@ class Crawler(object):
         statements = []
 
         if asn['asnName']:
-                statements.append([self.wh.get_pid('name'), asn['asnName'], self.reference])
+            name_qid = self.iyp.get_node('NAME', {'name': asn['asnName']}, create=True) 
+            statements.append([ 'NAME', name_qid, self.reference])
 
         # set countries
         cc = asn['country']['iso']
         if cc:
-            statements.append([ self.wh.get_pid('country'), self.wh.country2qid(cc), self.reference])
+            cc_qid = self.iyp.get_node('COUNTRY', {'country_code': cc}, create=True)
+            statements.append([ 'COUNTRY', cc_qid, self.reference])
 
         # set rank
-        statements.append([ self.wh.get_pid('ranking'), {
-            'amount': asn['rank'], 
-            'unit': self.asrank_qid,
-            },
-            self.reference])
+        statements.append([ 'RANK', 
+            self.asrank_qid, 
+            dict({'rank': asn['rank']}, **self.reference), 
+            ])
 
-        # Commit to wikibase
-        # Get the AS QID (create if AS is not yet registered) and commit changes
-        net_qid = self.wh.asn2qid(asn['asn'], create=True) 
-        self.wh.upsert_statements('update from CAIDA ASRank', net_qid, statements )
+        # Commit to iyp
+        # Get the AS (create if AS is not yet registered) and commit changes
+        asn_qid = self.iyp.get_node('AS', {'asn': asn['asn']}, create=True) 
+        self.iyp.add_links( asn_qid, statements )
         
 # Main program
 if __name__ == '__main__':
