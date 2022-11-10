ec915726079cae42abfc1232cc0091a27042618b
romain.fontugne <romain.fontugne@gmail.com>
2022-11-10 21:45:02 +0900

    rename crawler for RIPE AS names

--- iyp/crawlers/ripe/as_names.py
+++ iyp/crawlers/ripe/as_names.py
@@ -1,53 +1,56 @@
 import sys
 import logging
 import requests
-from iyp.wiki.wikihandy import Wikihandy
-#from ...lib.wikihandy import Wikihandy
+from datetime import datetime, time
+from iyp import IYP
 
-URL_RIPE_AS_NAME = 'https://ftp.ripe.net/ripe/asnames/asn.txt'
+URL = 'https://ftp.ripe.net/ripe/asnames/asn.txt'
+ORG = 'RIPE NCC'
 
 class Crawler(object):
     def __init__(self):
 
-        # Helper for wiki access
-        self.wh = Wikihandy()
-
         # Reference information for data pushed to the wikibase
-        self.reference = [
-            (self.wh.get_pid('source'), self.wh.get_qid('RIPE NCC')),
-            (self.wh.get_pid('reference URL'), URL_RIPE_AS_NAME),
-            (self.wh.get_pid('point in time'), self.wh.today())
-            ]
+        self.reference = {
+            'source': ORG,
+            'reference_url': URL,
+            'point_in_time': datetime.combine(datetime.utcnow(), time.min)
+            }
+
+        # connection to IYP database
+        self.iyp = IYP()
+
 
     def run(self):
         """Fetch the AS name file from RIPE website and process lines one by one"""
 
-        req = requests.get(URL_RIPE_AS_NAME)
+        req = requests.get(URL)
         if req.status_code != 200:
             sys.exit('Error while fetching AS names')
 
-        self.wh.login() # Login once for all threads, not needed with OAuth
-        
-        for i, res in enumerate(map(self.update_asn, req.text.splitlines())):
+        for i, _ in enumerate(map(self.update_asn, req.text.splitlines())):
             sys.stderr.write(f'\rProcessed {i} ASes')
-            
+
+        sys.stderr.write('\n')
+        self.iyp.close()
 
     def update_asn(self, one_line):
         # Parse given line to get ASN, name, and country code 
         asn, _, name_cc = one_line.partition(' ')
         name, _, cc = name_cc.rpartition(', ')
 
-        asn_qid = self.wh.asn2qid(asn, create=True)
-        cc_qid = self.wh.country2qid(cc, create=True)
+        asn_qid = self.iyp.get_node('AS', {'asn': asn}, create=True)
+        cc_qid = self.iyp.get_node('COUNTRY', {'country_code': cc}, create=True)
+        name_qid = self.iyp.get_node('NAME', {'name': name}, create=True)
 
         statements = []
-        statements.append( [self.wh.get_pid('country'), cc_qid, self.reference] )  # Set country
+        statements.append( ['COUNTRY', cc_qid, self.reference] )  # Set country
         if cc_qid is not None:
-            statements.append( [self.wh.get_pid('name'), name, self.reference] )       # Set AS name
+            statements.append( ['NAME', name_qid, self.reference] ) # Set AS name
 
         try:
             # Update AS name and country
-            self.wh.upsert_statements('updates from RIPE AS names', asn_qid, statements)
+            self.iyp.add_links(asn_qid, statements)
 
         except Exception as error:
             # print errors and continue running
