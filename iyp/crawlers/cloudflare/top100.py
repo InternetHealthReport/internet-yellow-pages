828e3f10580cd46c01051d90981c65ae3f60619c
romain.fontugne <romain.fontugne@gmail.com>
2022-11-21 15:48:50 +0900

    add cloudflare top 100 domains

---
+++ iyp/crawlers/cloudflare/top100.py
@@ -0,0 +1,64 @@
+import sys
+import logging
+import requests
+from iyp import BaseCrawler
+
+# Organization name and URL to data
+ORG = 'Cloudflare'
+URL = 'https://radar.cloudflare.com/charts/TopDomainsTable/attachment?location=&range=7d'
+
+class Crawler(BaseCrawler):
+    # Base Crawler provides access to IYP via self.iyp
+    # and setup a dictionary with the org/url/today's date in self.reference
+
+    def run(self):
+        """Fetch data and push to IYP. """
+
+        self.cf_qid = self.iyp.get_node(
+                'RANKING', {'name': f'Cloudflare top 100 domains'}, create=True)
+
+        # Fetch data
+        req = requests.get(self.reference['reference_url'])
+        if req.status_code != 200:
+            logging.error('Cannot download data {req.status_code}: {req.text}')
+            sys.exit('Error while fetching data file')
+
+        # Process line one after the other
+        for i, _ in enumerate(map(self.update, req.text.splitlines())):
+            sys.stderr.write(f'\rProcessed {i} lines')
+        sys.stderr.write('\n')
+    
+    def update(self, one_line):
+        """Add the entry to IYP if it's not already there and update its
+        properties."""
+
+        rank, domain = one_line.split(',')
+
+        if rank.startswith('Top'):
+            # Skip header
+            return
+
+        # set rank
+        statements = [[ 'RANK', self.cf_qid, dict({'rank': rank}, **self.reference) ]]
+
+        # Commit to IYP
+        # Get the AS's node ID (create if it is not yet registered) and commit changes
+        domain_qid = self.iyp.get_node('DOMAIN_NAME', {'name': domain}, create=True) 
+        self.iyp.add_links( domain_qid, statements )
+        
+# Main program
+if __name__ == '__main__':
+
+    scriptname = sys.argv[0].replace('/','_')[0:-3]
+    FORMAT = '%(asctime)s %(processName)s %(message)s'
+    logging.basicConfig(
+            format=FORMAT, 
+            filename='log/'+scriptname+'.log',
+            level=logging.WARNING, 
+            datefmt='%Y-%m-%d %H:%M:%S'
+            )
+    logging.info("Started: %s" % sys.argv)
+
+    crawler = Crawler(ORG, URL)
+    crawler.run()
+    crawler.close()
