78e44cca4a337137f9922cd406188d03c700ed4a
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-03-30 17:16:58 +0530

    implemented error handling

--- send_email.py
+++ send_email.py
@@ -10,41 +10,45 @@ with open('config.json', 'r') as fp:
 
 
 def send_email(e):
-    # Send an email
     email_config = conf['email']
-    sender = email_config['email_address']
-    recipient = sender
-    smtp_server = email_config['smtp_server']
-    smtp_port = email_config['smtp_port']
-    username = email_config['username']
-    password = email_config['password']
+    if email_config is not None and email_config['email_address'] != "" and email_config['smtp_server'] != "" and email_config['smtp_port'] != "" and email_config['username'] != "" and email_config['password'] != "":
+        # Send an email
+        sender = email_config['email_address']
+        recipient = sender
+        smtp_server = email_config['smtp_server']
+        smtp_port = email_config['smtp_port']
+        username = email_config['username']
+        password = email_config['password']
 
-    msg = EmailMessage()
-    msg['From'] = formataddr(('Internet Yellow Pages.', f'{sender}'))
-    msg['To'] = recipient
-    msg['Subject'] = 'Error when running create_db script'
-    msg.set_content(
-        f"""\
-        An exception was caught during run of create_db script:\n\n{str(e)}
-        """
-    )
-    msg.add_alternative(
-        f"""\
-        <html>
-          <body>
-            <p>An exception was caught during run of create_db script:</p>
-            <p>{str(e)}</p>
-          </body>
-        </html>
-        """,
-        subtype="html",
-    )
+        msg = EmailMessage()
+        msg['From'] = formataddr(('Internet Yellow Pages.', f'{sender}'))
+        msg['To'] = recipient
+        msg['Subject'] = 'Error when running create_db script'
+        msg.set_content(
+            f"""\
+                An exception was caught during run of create_db script:\n\n{str(e)}
+                """
+        )
+        msg.add_alternative(
+            f"""\
+                <html>
+                  <body>
+                    <p>An exception was caught during run of create_db script:</p>
+                    <p>{str(e)}</p>
+                  </body>
+                </html>
+                """,
+            subtype="html",
+        )
 
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(sender, recipient, msg.as_string())
-        logging.info("Email sent")
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(sender, recipient, msg.as_string())
+            logging.info("Email sent")
+            server.quit()
+    else:
+        print('Email credentials not found in config file')
 
 
 if __name__ == "__main__":
