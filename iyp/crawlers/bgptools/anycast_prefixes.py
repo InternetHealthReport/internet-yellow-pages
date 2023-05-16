dd6a97418db255c4fb044caa573785ca463b9b71
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-05-16 07:08:29 +0530

    Refactored according to the review.

--- iyp/crawlers/bgptools/anycast_prefixes.py
+++ iyp/crawlers/bgptools/anycast_prefixes.py
@@ -13,7 +13,7 @@ URL = 'https://github.com/bgptools/anycast-prefixes'
 NAME = 'bgptools.anycast_prefixes'  # should reflect the directory and name of this file
 
 
-def get_latest_dataset_url(as_prefixes_data_url: str, ip_version: int):
+def get_dataset_url(as_prefixes_data_url: str, ip_version: int):
     anycast_prefixes_data_url_formatted: str = as_prefixes_data_url.replace('github.com', 'raw.githubusercontent.com')
     if ip_version == 4:
         anycast_prefixes_data_url_formatted += '/master/anycatch-v4-prefixes.txt'
@@ -39,8 +39,8 @@ class Crawler(BaseCrawler):
     # and setup a dictionary with the org/url/today's date in self.reference
 
     def run(self):
-        ipv4_prefixes_url = get_latest_dataset_url(URL, 4)
-        ipv6_prefixes_url = get_latest_dataset_url(URL, 6)
+        ipv4_prefixes_url = get_dataset_url(URL, 4)
+        ipv6_prefixes_url = get_dataset_url(URL, 6)
 
         # Create a temporary directory
         tmpdir = tempfile.mkdtemp()
@@ -50,14 +50,17 @@ class Crawler(BaseCrawler):
         ipv6_prefixes_filename = os.path.join(tmpdir, 'anycast_ipv6_prefixes.txt')
 
         # Fetch data and push to IYP.
+        self.reference['reference_url'] = ipv4_prefixes_url  # Overriding the reference_url according to prefixes
         ipv4_prefixes_response = fetch_dataset(ipv4_prefixes_url)
-        logging.info('IPV4 prefixes fetched successfully.')
+        logging.info('IPv4 prefixes fetched successfully.')
         self.update(ipv4_prefixes_response, ipv4_prefixes_filename)
-        logging.info('IPV4 prefixes pushed to IYP.')
+        logging.info('IPv4 prefixes pushed to IYP.')
+
+        self.reference['reference_url'] = ipv6_prefixes_url
         ipv6_prefixes_response = fetch_dataset(ipv6_prefixes_url)
-        logging.info('IPV6 prefixes fetched successfully.')
+        logging.info('IPv6 prefixes fetched successfully.')
         self.update(ipv6_prefixes_response, ipv6_prefixes_filename)
-        logging.info('IPV6 prefixes pushed to IYP.')
+        logging.info('IPv6 prefixes pushed to IYP.')
 
     def update(self, res, filename: str):
         with open(filename, 'w') as file:
