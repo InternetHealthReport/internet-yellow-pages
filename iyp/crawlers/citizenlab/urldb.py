a075ba228e606091cab7ee9349cfca5509584fe0
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-08-14 12:02:49 +0530

    fix: citizenlab/urldb.py crawler (#63)
    * fix: citizenlab/urldb.py crawler
    
    * fix: lint

--- iyp/crawlers/citizenlab/urldb.py
+++ iyp/crawlers/citizenlab/urldb.py
@@ -1,10 +1,10 @@
 import argparse
+import csv
 import logging
 import os
 import sys
 
 import requests
-from bs4 import BeautifulSoup
 
 from iyp import BaseCrawler
 
@@ -15,7 +15,7 @@ NAME = 'citizenlab.urldb'  # should reflect the directory and name of this file
 
 
 def generate_url(suffix):
-    base_url = URL
+    base_url = 'https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/'
     joined_url = ''.join([base_url, suffix, '.csv'])
     return joined_url
 
@@ -25,46 +25,49 @@ class Crawler(BaseCrawler):
     # and set up a dictionary with the org/url/today's date in self.reference
 
     def run(self):
-        """Fetch data and push to IYP."""
-
-        # Fetch country code
-        url_for_country_codes = generate_url('00-LEGEND-country_codes')
-        req_for_country_codes = requests.get(url_for_country_codes)
+        # Fetch country codes to generate urls
+        req_for_country_codes = requests.get(generate_url('00-LEGEND-country_codes'))
 
         if req_for_country_codes.status_code != 200:
             logging.error('Cannot download data {req.status_code}: {req.text}')
             sys.exit('Error while fetching data file')
 
-        soup = BeautifulSoup(req_for_country_codes.content, 'html.parser')
-        rows = soup.find_all('tr', class_='js-file-line')
+        content = req_for_country_codes.content.decode('utf-8')
+        csv_data = csv.reader(content.splitlines(), delimiter=',')
 
         country_codes = []
-        for row in rows:
-            country_code = row.select_one(':nth-child(2)')
-            if len(country_code.text) != 2:
-                continue
-            country_codes.append(country_code.text.lower())
+        for row in csv_data:
+            country_codes.append(row[0].lower())
 
-        # Fetch from each csv file using country code
+        # Iterate through country_codes, generate an url, download the csv file,
+        # extract the necessary information from the csv file,
+        # and push the data to IYP.
         lines = []
         urls = set()
         categories = set()
+
         for code in country_codes:
             url = generate_url(code)
             req_with_respect_to_country_code = requests.get(url)
+            print('Processing {}'.format(code))
+
+            # Not necessarily every country code have a csv file.
+            # Skipping those don't have one.
             if req_with_respect_to_country_code.status_code != 200:
+                print('Skipping {}'.format(code))
                 continue
-            country_soup = BeautifulSoup(req_with_respect_to_country_code.content, 'html.parser')
-            rows = country_soup.find_all('tr', class_='js-file-line')
+
+            decoded_response = req_with_respect_to_country_code.content.decode('utf-8')
+            rows = csv.reader(decoded_response.splitlines(), delimiter=',')
             for row in rows:
-                url = row.select_one(':nth-child(2)').text
-                # category_code = row.select_one(":nth-child(3)").text
-                category = row.select_one(':nth-child(4)').text
+                url = row[0]
+                category = row[2]
                 urls.add(url)
                 categories.add(category)
                 if [url, category] in lines:
                     continue
                 lines.append([url, category])
+
         url_id = self.iyp.batch_get_nodes('URL', 'url', urls)
         category_id = self.iyp.batch_get_nodes('Tag', 'label', categories)
 
@@ -72,10 +75,11 @@ class Crawler(BaseCrawler):
         for (url, category) in lines:
             url_qid = url_id[url]
             category_qid = category_id[category]
-            links.append({'src_id': url_qid, 'dst_id': category_qid, 'props': [self.reference]})  # Set URL category
+            links.append({'src_id': url_qid, 'dst_id': category_qid, 'props': [self.reference]})
 
         # Push all links to IYP
         self.iyp.batch_add_links('CATEGORIZED', links)
+        print('Processed citizenlab/test-lists repo.')
 
 
 def main() -> None:
