95f40ec67400b29d924c19d6f91b74a0eb327bd9
romain.fontugne <romain.fontugne@gmail.com>
2023-12-21 12:35:01 +0900

    Split IPv4 and IPv6 data from BGPKit's AS relationships dataset (as2rel)

---
+++ iyp/crawlers/bgpkit/__init__.py
@@ -0,0 +1,46 @@
+import bz2
+import json
+import sys
+
+import requests
+
+from iyp import BaseCrawler
+
+
+class AS2RelCrawler(BaseCrawler):
+    def __init__(self, organization, url, name, af):
+        """Initialization: set the address family attribute (af)"""
+
+        self.af = af
+        super().__init__(organization, url, name)
+
+    def run(self):
+        """Fetch the AS relationship file from BGPKIT website and process lines one by
+        one."""
+
+        req = requests.get(self.url, stream=True)
+        if req.status_code != 200:
+            sys.exit('Error while fetching AS relationships')
+
+        rels = []
+        asns = set()
+
+        # Collect all ASNs
+        for rel in json.load(bz2.open(req.raw)):
+            asns.add(rel['asn1'])
+            asns.add(rel['asn2'])
+            rels.append(rel)
+
+        # get ASNs IDs
+        self.asn_id = self.iyp.batch_get_nodes_by_single_prop('AS', 'asn', asns)
+
+        # Compute links
+        links = []
+        for rel in rels:
+            as1_qid = self.asn_id[rel['asn1']]
+            as2_qid = self.asn_id[rel['asn2']]
+
+            links.append({'src_id': as1_qid, 'dst_id': as2_qid, 'props': [self.reference, rel], 'af': self.af})
+
+        # Push all links to IYP
+        self.iyp.batch_add_links('PEERS_WITH', links)
---
+++ iyp/crawlers/bgpkit/as2rel_v4.py
@@ -0,0 +1,47 @@
+import argparse
+import logging
+import os
+import sys
+
+from iyp.crawlers.bgpkit import AS2RelCrawler
+
+URL = 'https://data.bgpkit.com/as2rel/as2rel-v4-latest.json.bz2'
+ORG = 'BGPKIT'
+NAME = 'bgpkit.as2rel_v4'
+
+AF = 4
+
+
+class Crawler(AS2RelCrawler):
+    def __init__(self, organization, url, name):
+        super().__init__(organization, url, name, AF)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--unit-test', action='store_true')
+    args = parser.parse_args()
+
+    scriptname = os.path.basename(sys.argv[0]).replace('/', '_')[0:-3]
+    FORMAT = '%(asctime)s %(levelname)s %(message)s'
+    logging.basicConfig(
+        format=FORMAT,
+        filename='log/' + scriptname + '.log',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    logging.info(f'Started: {sys.argv}')
+
+    crawler = Crawler(ORG, URL, NAME)
+    if args.unit_test:
+        crawler.unit_test(logging)
+    else:
+        crawler.run()
+        crawler.close()
+    logging.info(f'Finished: {sys.argv}')
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
