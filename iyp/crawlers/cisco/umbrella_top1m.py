4a752564a67d2d719d51390ba971c2e661a48974
romain.fontugne <romain.fontugne@gmail.com>
2024-03-04 15:48:39 +0900

    all files and reference names should be exactly the same and lower case

--- iyp/crawlers/cisco/umbrella_top1m.py
+++ iyp/crawlers/cisco/umbrella_top1m.py
@@ -14,7 +14,7 @@ from iyp import BaseCrawler, RequestStatusError
 # URL to umbrella top 1M
 URL = 'http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip'
 ORG = 'Cisco'
-NAME = 'cisco.umbrella_top1M'
+NAME = 'cisco.umbrella_top1m'
 
 
 class Crawler(BaseCrawler):
