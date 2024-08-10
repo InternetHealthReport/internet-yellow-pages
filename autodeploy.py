d0c6d86e708e4365242bdceb11a091ceccec57c9
Ryan Poon <ryanpoon18@gmail.com>
2024-08-10 01:51:37 +0000

    clean up code and check active deployment and archive for date to deploy

--- autodeploy.py
+++ autodeploy.py
@@ -6,8 +6,12 @@ import logging
 import requests
 import time
 import json
+import arrow
+import datetime
 
 NEO4J_VERSION = '5.16.0'
+today = arrow.utcnow()
+root = os.path.dirname(os.path.realpath(__file__))
 
 
 FORMAT = '%(asctime)s %(processName)s %(message)s'
@@ -19,48 +23,111 @@ logging.basicConfig(
 logging.warning('Starting deployment')
 
 client = docker.from_env()
+def remove_deployment(month, day):
+    """Checks if there is an active deployment for the given month and day. If there is,
+    remove it and the corresponding volume storing its data.
+    """
+    try:
+        container = client.containers.get(f'deploy-{month}-{day}')
+        logging.warning(f'Removing active deployment for {month}-{day}')
+        container.stop()
+        # Wait a little bit after the container has been removed before deleting the volume
+        while True:
+            try:
+                client.volumes.get(f'data-{month}-{day}').remove()
+                break
+            except:
+                time.sleep(0.1)
+    except docker.errors.NotFound:
+        logging.warning(f'No existing deployment for {date}. Starting deployment')
 
-date = sys.argv[1]
-year, month, day = date.split('-')
-root = os.path.dirname(os.path.realpath(__file__))
 
+def get_config_ports():
+    """Makes a request to caddy config and returns the ports current being used (both active and previous)"""
+    with requests.get("http://sandbox.ihr.live:2019/config/") as response:
+        body = json.loads(response.content.decode())
+        routes = body['apps']['http']['servers']['srv0']['routes']
+        active = {}
+        try:
+            for route in routes:
+                host = route['match'][0]['host'][0]
+                dial = route['handle'][0]['routes'][0]['handle'][0]['upstreams'][0]['dial']
+                active[host] = dial
+            active_bolt_port = active['ryan-bolt.ihr.live'].split(':')[1]
+            active_http_port = active['ryan.ihr.live'].split(':')[1]
+            prev_http_port = active['ryan-prev.ihr.live'].split(':')[1]
+            prev_bolt_port = active['ryan-prev-bolt.ihr.live'].split(':')[1]
+            return {
+                'active_bolt': active_bolt_port,
+                'active_http': active_http_port,
+                'prev_http': prev_http_port,
+                'prev_bolt': prev_bolt_port
+            }
+        except:
+            logging.warning('Unable to find currently active deployments')
+            return None
+        
+
+def check_log(year, month, day):
+    """Makes a request to archive and checks if there is a valid dump for the specified date"""
+    date = f'{year}-{month}-{day}'
+    logging.warning(f'Downloading logs for {date}')
+    with requests.get(f'https://ihr-archive.iijlab.net/ihr-dev/iyp/{year}/{month}/{day}/iyp-{date}.log') as response:
+        if response.status_code == 200:
+            body = response.content
+            last_line = body.decode().split('\n')[-1]
+            if 'Errors:' in last_line:
+                logging.warning(f'There were errors from create_db found in logs for iyp-{date}.log')
+                sys.exit(1)
+            return True
+        return False
+
+        
+def get_port_date(port):
+    """Extracts the month and day from a port. Returns the tuple (month, day)"""
+    month = port[-4:-2]
+    day = port[-2:]
+    return month, day
+
+# If no date is provided when running the script, check if any dumps have been made within a week since the
+# previous deployment. Otherwise, use the date provided in command line arg.
+if len(sys.argv) < 2:
+    ports = get_config_ports()
+    if ports:
+        active_http = ports['active_http']
+        month, day = get_port_date(active_http)
+        start_date = datetime.date(int(today.year), int(month), int(day))
+        success = False
+    
+    # Download logs from ihr archive each day in the next week since the previous release
+    for i in range(1,8):
+        date = start_date + datetime.timedelta(days=i)
+        date = date.strftime('%Y-%m-%d')
+        logging.warning(f'Checking archive for {date}')
+        year, month, day = date.split('-')
+        if check_log(year, month, day):
+            success = True
+            break
+        else:
+            logging.warning(f'No archive entry found for {date}')
+
+    if not success:
+        logging.warning('Exiting because no active dates were found in caddy')
+        sys.exit(1)
+else:
+    date = sys.argv[1]
+    year, month, day = sys.argv[1].split('-')
+
+
+# Define ports and filenames that depend on the date
 neo4j_volume = f'data-{month}-{day}'
 deploy_name = f'deploy-{month}-{day}'
 gui_port = f'1{month}{day}'
 bolt_port = f'2{month}{day}'
 
 
-def remove_deployment(month, day):
-    container = client.containers.get(f'deploy-{month}-{day}')
-    logging.warning(f'Removing active deployment for {month}-{day}')
-    container.stop()
-    # Wait a little bit after the container has been removed before deleting the volume
-    while True:
-        try:
-            client.volumes.get(f'data-{month}-{day}').remove()
-            break
-        except:
-            time.sleep(0.1)
-
-try: 
-    remove_deployment(month, day)
-except docker.errors.NotFound as exc:
-    logging.warning(f'No existing deployment for {date}. Starting deployment')
-
-# Alternatively, specify your own path.
-# root = ''
-if not root:
-    sys.exit('Please configure a root path.')
-
-
-# Download logs from ihr archive
-logging.warning(f'Downloading logs for {date}')
-with requests.get(f'https://ihr-archive.iijlab.net/ihr-dev/iyp/{year}/{month}/{day}/iyp-{date}.log') as response:
-    body = response.content
-    last_line = str(body).split('\n')[-1]
-    if 'Errors:' in last_line:
-        print('There were errors from create_db found in logs')
-        sys.exit(1)
+# Check if there is an existing deployment for this day and remove if so
+remove_deployment(month, day)
 
 
 # Download dump from ihr archive
@@ -87,9 +154,9 @@ container = client.containers.run(
         }
     )
 
+
 # Run neo4j based on data in volume just created
 logging.warning('Starting deployment container')
-
 container = client.containers.run(
     'neo4j:' + NEO4J_VERSION,
     name=deploy_name,
@@ -109,42 +176,24 @@ container = client.containers.run(
     remove=True
 )
 
-# # Get currently active config
-response = requests.get("http://sandbox.ihr.live:2019/config/")
-body = json.loads(response.content)
-routes = body['apps']['http']['servers']['srv0']['routes']
-active = {}
-current_active_bolt_port = ''
-current_active_http_port = ''
-try:
-    for route in routes:
-        host = route['match'][0]['host'][0]
-        dial = route['handle'][0]['routes'][0]['handle'][0]['upstreams'][0]['dial']
-        active[host] = dial
 
-    current_active_bolt_port = active['ryan-bolt.ihr.live'].split(':')[1]
-    current_active_http_port = active['ryan.ihr.live'].split(':')[1]
-
-    prev_http_port = active['ryan-prev.ihr.live'].split(':')[1]
-    prev_day = prev_http_port[-2:]
-    prev_month = prev_http_port[-4:-2]
-
-    # It's possible to that you're trying to redeploy the current prev
-    # If this condition isn't here, then the new deployment will be deleted
-    # since it has the same date as prev
-    if prev_month != month or prev_day != day:
-        remove_deployment(prev_month, prev_day)
-except:
-    print('Unable to find currently active deployments')
-
+# Get currently active config
+ports = get_config_ports()
+prev_month, prev_day = get_port_date(ports['prev_http'])
+# It's possible that you're trying to redeploy the current prev
+# If this condition isn't here, then the new deployment will be deleted
+# since it has the same date as prev
+if prev_month != month or prev_day != day:
+    remove_deployment(prev_month, prev_day)
 
 with open('caddy.json', 'r') as f:
     json = f.read()
 
 json = json.replace('<BOLT_PORT>', bolt_port)
 json = json.replace('<HTTP_PORT>', gui_port)
-json = json.replace('<PREV_BOLT_PORT>', current_active_bolt_port)
-json = json.replace('<PREV_HTTP_PORT>', current_active_http_port)
+json = json.replace('<PREV_BOLT_PORT>', ports['active_bolt'])
+json = json.replace('<PREV_HTTP_PORT>', ports['active_http'])
+
 
 # Update config
 requests.post('http://localhost:2019/load', json, headers={'Content-Type': 'application/json'})
