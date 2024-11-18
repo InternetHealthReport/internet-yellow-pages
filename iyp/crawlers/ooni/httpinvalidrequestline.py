8f7146ed075826c2a0ca39655811ef45828762ba
Malte Tashiro <malte@iij.ad.jp>
2024-11-18 07:44:07 +0000

    Update OONI crawlers.

--- iyp/crawlers/ooni/httpinvalidrequestline.py
+++ iyp/crawlers/ooni/httpinvalidrequestline.py
@@ -17,99 +17,50 @@ class Crawler(OoniCrawler):
 
     def __init__(self, organization, url, name):
         super().__init__(organization, url, name, 'httpinvalidrequestline')
+        self.categories = ['tampering', 'no_tampering']
 
     def process_one_line(self, one_line):
         """Process a single line from the jsonl file and store the results locally."""
-        super().process_one_line(one_line)
+        if super().process_one_line(one_line):
+            return
 
-        tampering = one_line.get('test_keys', {}).get('tampering', False)
+        tampering = 'tampering' if one_line['test_keys']['tampering'] else 'no_tampering'
 
         # Using the last result from the base class, add our unique variables
-        self.all_results[-1] = self.all_results[-1][:2] + (tampering,)
+        self.all_results[-1] = self.all_results[-1] + (tampering,)
 
     def batch_add_to_iyp(self):
         super().batch_add_to_iyp()
 
         httpinvalidrequestline_id = self.iyp.get_node('Tag', {'label': label}, create=True)
 
-        censored_links = []
+        censored_links = list()
 
-        # Accumulate properties for each ASN-country pair
-        link_properties = defaultdict(lambda: defaultdict(int))
+        # Create one link per ASN-country pair.
+        for (asn, country), result_dict in self.all_percentages.items():
+            asn_id = self.node_ids['asn'][asn]
+            props = dict()
+            for category in self.categories:
+                props[f'percentage_{category}'] = result_dict['percentages'][category]
+                props[f'count_{category}'] = result_dict['category_counts'][category]
+            props['total_count'] = result_dict['total_count']
+            props['country_code'] = country
+            censored_links.append(
+                {'src_id': asn_id, 'dst_id': httpinvalidrequestline_id, 'props': [props, self.reference]}
+            )
 
-        for asn, country, tampering in self.all_results:
-            asn_id = self.node_ids['asn'].get(asn)
-            country_id = self.node_ids['country'].get(country)
-
-            if asn_id and country_id:
-                props = self.reference.copy()
-                if (asn, country) in self.all_percentages:
-                    percentages = self.all_percentages[(asn, country)].get(
-                        'percentages', {}
-                    )
-                    counts = self.all_percentages[(asn, country)].get(
-                        'category_counts', {}
-                    )
-                    total_count = self.all_percentages[(asn, country)].get(
-                        'total_count', 0
-                    )
-                    props['percentage_no_tampering'] = percentages.get(
-                        'no_tampering', 0
-                    )
-                    props['count_no_tampering'] = counts.get('no_tampering', 0)
-                    props['percentage_tampering'] = percentages.get('tampering', 0)
-                    props['count_tampering'] = counts.get('tampering', 0)
-                    props['total_count'] = total_count
-
-                # Accumulate properties
-                link_properties[(asn_id, httpinvalidrequestline_id)] = props
-
-        for (asn_id, httpinvalidrequestline_id), props in link_properties.items():
-            if (asn_id, httpinvalidrequestline_id) not in self.unique_links['CENSORED']:
-                self.unique_links['CENSORED'].add((asn_id, httpinvalidrequestline_id))
-                censored_links.append(
-                    {
-                        'src_id': asn_id,
-                        'dst_id': httpinvalidrequestline_id,
-                        'props': [props],
-                    }
-                )
-
-        # Batch add the links (this is faster than adding them one by one)
         self.iyp.batch_add_links('CENSORED', censored_links)
 
-    def calculate_percentages(self):
+    def aggregate_results(self):
         target_dict = defaultdict(lambda: defaultdict(int))
-
-        # Initialize counts for all categories
-        categories = ['tampering', 'no_tampering']
 
         # Populate the target_dict with counts
         for entry in self.all_results:
             asn, country, tampering = entry
-            target_dict[(asn, country)]['tampering'] += 1 if tampering else 0
-            target_dict[(asn, country)]['no_tampering'] += 1 if not tampering else 0
-
-        self.all_percentages = {}
+            target_dict[(asn, country)][tampering] += 1
 
         for (asn, country), counts in target_dict.items():
-            total_count = counts['tampering'] + counts['no_tampering']
-            for category in categories:
-                counts[category] = counts.get(category, 0)
-
-            percentages = {
-                category: (
-                    (counts[category] / total_count) * 100 if total_count > 0 else 0
-                )
-                for category in categories
-            }
-
-            result_dict = {
-                'total_count': total_count,
-                'category_counts': dict(counts),
-                'percentages': percentages,
-            }
-            self.all_percentages[(asn, country)] = result_dict
+            self.all_percentages[(asn, country)] = self.make_result_dict(counts)
 
     def unit_test(self):
         return super().unit_test(['CENSORED'])
