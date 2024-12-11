0a60022d610791fe6d06fafdca387f80a58a4245
Malte Tashiro <malte@iij.ad.jp>
2024-12-11 02:46:19 +0000

    Refactor
    - Create everything in one batch
    - Fix: reference time now points to the first day of the month at
      midnight
    - Fix: reference time needs to be a datetime object, not Arrow
    - Fix: fetch the newest data, not the oldest
    - Make data URL more precise
    - Add country code to RANK relationship
    - Use itertuples instead of iterrows since it is faster
    - Add COUNTRY relationship check to unit test
    - Change log level to INFO
    - Fix typos and consistency in README

--- iyp/crawlers/google/README.md
+++ iyp/crawlers/google/README.md
@@ -1,18 +1,18 @@
-# Google CrUX
+# Google CrUX -- https://developer.chrome.com/docs/crux
 
 The Chrome User Experience Report (CrUX for short) is a dataset collected by
-Google that reflects how real-world Chrome users experience popular 
+Google that reflects how real-world Chrome users experience popular
 destinations on the web.
 
-CrUX data is collected from real browsers around the world, based on certain 
-browser options which determine user eligibility. A set of dimensions and metrics 
+CrUX data is collected from real browsers around the world, based on certain
+browser options which determine user eligibility. A set of dimensions and metrics
 are collected which allow site owners to determine how users experience their sites.
 
 IYP fetches CrUX's [top 1M popular websites per country](https://github.com/InternetHealthReport/crux-top-lists-country).
-Unlike others, CrUX rankings are buketed by rank magnitude order, not by
-specific rank. For example, rank are 1000, 10k, 100k, or 1M.
+Unlike others, CrUX rankings are bucketed by rank magnitude order, not by
+specific rank. For example, ranks are 1000, 10k, 100k, or 1M.
 
-In addition, CrUX ranks *origins* (e.g. https://www.google.com), not domain
+In addition, CrUX ranks *origins* (e.g., https://www.google.com), not domain
 or host names. In IYP we extract the hostname part of the origin and model this
 dataset using the hostname.
 
--- iyp/crawlers/google/crux_top1m_country.py
+++ iyp/crawlers/google/crux_top1m_country.py
@@ -2,24 +2,22 @@ import argparse
 import logging
 import os
 import sys
-from datetime import timezone
 from urllib.error import HTTPError
 
 import arrow
+import iso3166
 import pandas as pd
-from iso3166 import countries
 
 from iyp import BaseCrawler
 
 # Data source Google (archived on github by IHR)
 ORG = 'Google'
-URL = 'https://github.com/InternetHealthReport/crux-top-lists-country/main/data/country/'
+URL = 'https://github.com/InternetHealthReport/crux-top-lists-country/raw/refs/heads/main/data/country'
 NAME = 'google.crux_top1m_country'
 
 
 def generate_url(country_code, date):
-    base_url = 'https://github.com/InternetHealthReport/crux-top-lists-country/raw/refs/heads/main/data/country/'
-    joined_url = os.path.join(base_url, country_code.lower(), f'{date.year}{date.month:02d}.csv.gz')
+    joined_url = os.path.join(URL, country_code.lower(), f'{date.year}{date.month:02d}.csv.gz')
     return joined_url
 
 
@@ -31,59 +29,86 @@ class Crawler(BaseCrawler):
     def run(self):
         """Fetch data and push to IYP."""
 
-        for country in countries:
+        rankings = set()
+        hostnames = set()
+        countries = set()
+
+        country_links = list()
+        rank_links = list()
+        for country in iso3166.countries:
             country_code = country.alpha2
 
-            start = arrow.now().shift(months=-3)
-            end = arrow.now()
+            end = arrow.utcnow().replace(day=1, hour=0, minute=0, microsecond=0)
+            start = end.shift(months=-3)
             df = None
 
             for date in reversed(list(arrow.Arrow.range('month', start, end))):
                 # Fetch data
                 try:
-                    df = pd.read_csv(generate_url(country_code, date))
+                    url = generate_url(country_code, date)
+                    df = pd.read_csv(url)
 
                     # Set the modification time to the time of the dump
-                    self.reference['reference_time_modification'] = date.replace(tzinfo=timezone.utc)
-
+                    self.reference['reference_time_modification'] = date.datetime
+                    self.reference['reference_url_data'] = url
+                    break
                 except HTTPError:
+                    # Data not available for this timestamp.
                     continue
 
             if df is None:
-                # this country is not in the dataset
+                # This country is not in the dataset or no data available within the
+                # timeframe.
                 continue
 
             # Extract hostname from "URLs"
             df['hostname'] = df['origin'].str.partition('://')[2]
 
+            ranking_name = f'CrUX top 1M ({country_code})'
+
             # Create/fetch corresponding nodes in IYP
-            nodes = set(df['hostname'].unique())
-            node_id = self.iyp.batch_get_nodes_by_single_prop('HostName', 'name', nodes, all=False)
-            ranking = self.iyp.get_node('Ranking', {'name': f'CrUX top 1M ({country_code})'})
-            country = self.iyp.get_node('Country', {'country_code': country_code}, ['country_code'])
+            hostnames.update(df['hostname'].unique())
+            rankings.add(ranking_name)
+            countries.add(country_code)
 
-            # Create the (:Ranking)-[:COUNTRY]-(:Country) relationship
-            self.iyp.add_links(ranking, [['COUNTRY', country, self.reference]])
+            country_links.append({
+                'src_id': ranking_name,
+                'dst_id': country_code,
+                'props': [
+                    self.reference.copy()
+                ]
+            })
 
-            # Compute hostname/ranking relationships
-            links = list()
-
-            for _, row in df.iterrows():
-                links.append({
-                    'src_id': node_id[row['hostname']],
-                    'dst_id': ranking,
+            for row in df.itertuples():
+                rank_links.append({
+                    'src_id': row.hostname,
+                    'dst_id': ranking_name,
                     'props': [
-                        self.reference,
-                        {'rank': row['rank'], 'origin': row['origin']}
+                        self.reference.copy(),
+                        {'rank': row.rank, 'origin': row.origin, 'country_code': country_code}
                     ]
                 })
 
-            # Create the (:HostName)-[:RANK]-(:Ranking) relationship
-            self.iyp.batch_add_links('RANK', links)
+        ranking_id = self.iyp.batch_get_nodes_by_single_prop('Ranking', 'name', rankings, all=False)
+        hostname_id = self.iyp.batch_get_nodes_by_single_prop('HostName', 'name', hostnames, all=False)
+        country_id = self.iyp.batch_get_nodes_by_single_prop('Country', 'country_code', countries, all=False)
+
+        for link in country_links:
+            link['src_id'] = ranking_id[link['src_id']]
+            link['dst_id'] = country_id[link['dst_id']]
+
+        for link in rank_links:
+            link['src_id'] = hostname_id[link['src_id']]
+            link['dst_id'] = ranking_id[link['dst_id']]
+
+        # Create the (:Ranking)-[:COUNTRY]-(:Country) relationship
+        self.iyp.batch_add_links('COUNTRY', country_links)
+        # Create the (:HostName)-[:RANK]-(:Ranking) relationship
+        self.iyp.batch_add_links('RANK', rank_links)
 
     def unit_test(self):
         # Unit test checks for existence of relationships created by this crawler.
-        return super().unit_test(['RANK'])
+        return super().unit_test(['COUNTRY', 'RANK'])
 
 
 def main() -> None:
@@ -96,7 +121,7 @@ def main() -> None:
     logging.basicConfig(
         format=FORMAT,
         filename='log/' + scriptname + '.log',
-        level=logging.WARNING,
+        level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
