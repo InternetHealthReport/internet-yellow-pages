c79ced8783c73f282d273a886f4aa5cf8018fdd9
mtashiro <malte@iij.ad.jp>
2025-05-30 15:53:48 +0900

    Add DNS Graph crawler for CRuX data (#185)

---
+++ iyp/crawlers/openintel/dnsgraph_crux.py
@@ -0,0 +1,43 @@
+import argparse
+import logging
+import sys
+
+from iyp.crawlers.openintel import DnsgraphCrawler
+
+URL = 'https://storage.dacs.utwente.nl/sommeser-dnsdep/CRUX'
+ORG = 'OpenINTEL'
+NAME = 'openintel.dnsgraph_crux'
+
+
+class Crawler(DnsgraphCrawler):
+    def __init__(self, organization, url, name):
+        super().__init__(organization, url, name)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--unit-test', action='store_true')
+    args = parser.parse_args()
+
+    FORMAT = '%(asctime)s %(levelname)s %(message)s'
+    logging.basicConfig(
+        format=FORMAT,
+        filename='log/' + NAME + '.log',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    logging.info(f'Started: {sys.argv}')
+
+    crawler = Crawler(ORG, URL, NAME)
+    if args.unit_test:
+        crawler.unit_test()
+    else:
+        crawler.run()
+        crawler.close()
+    logging.info(f'Finished: {sys.argv}')
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
