f7952756c13a39484fb1dc843e42628ba743eb24
Malte Tashiro <malte@iij.ad.jp>
2025-10-15 01:40:08 +0000

    Handle HTTP 429 error in Cloudflare crawler
    The Cloudflare API rate limit does not seem to be consistent and they do
    not include Retry-After headers as described in their documentation, so
    we sometimes get a Too Many Requests error.
    
    We now cancel outstanding requests and wait for five minutes before
    trying again.
    
    See also: https://developers.cloudflare.com/fundamentals/api/reference/limits/

--- iyp/crawlers/cloudflare/__init__.py
+++ iyp/crawlers/cloudflare/__init__.py
@@ -1,8 +1,9 @@
 import json
 import logging
 import os
-from concurrent.futures import as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime, timezone
+from time import sleep
 
 from requests.adapters import HTTPAdapter, Retry
 from requests_futures.sessions import FuturesSession
@@ -13,6 +14,7 @@ BATCH_SIZE = 10
 RANK_THRESHOLD = 10000
 TOP_LIMIT = 100
 PARALLEL_DOWNLOADS = 4
+MAX_RETRIES = 5
 
 API_KEY = str()
 if os.path.exists('config.json'):
@@ -54,37 +56,35 @@ class DnsTopCrawler(BaseCrawler):
         # ASNs for top_ases, country codes for top_locations.
         self.to_nodes = set()
         self.links = list()
+        # Clear the cache.
+        self.tmp_dir = self.create_tmp_dir()
+        self.batches = dict()
 
-    def fetch(self):
-        """Download data for top RANK_THRESHOLD domain names registered in IYP and save
-        it on disk."""
-
-        req_session = FuturesSession(max_workers=PARALLEL_DOWNLOADS)
+    def __init_session(self):
+        # The session.close() implementation of the FuturesSession does not actually
+        # cancel scheduled futures as described in the documentation, so we have to
+        # specify our own executor.
+        self.executor = ThreadPoolExecutor(PARALLEL_DOWNLOADS)
+        self.session = FuturesSession(executor=self.executor)
         # Set API authentication headers.
-        req_session.headers['Authorization'] = 'Bearer ' + API_KEY
-        req_session.headers['Content-Type'] = 'application/json'
+        self.session.headers['Authorization'] = 'Bearer ' + API_KEY
+        self.session.headers['Content-Type'] = 'application/json'
 
         retries = Retry(total=10,
                         backoff_factor=0.1,
                         status_forcelist=[500, 502, 503, 504])
 
-        req_session.mount('https://', HTTPAdapter(max_retries=retries))
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
 
-        # Clear the cache.
-        tmp_dir = self.create_tmp_dir()
-
-        queries = list()
-
+    def make_batches(self):
         # Query Cloudflare API in batches.
-        i = 0
         batch = list()
+        batch_id = 0
         fpaths = dict()
-        while i < len(self.names):
-            name = self.names[i]
-            i += 1
-            # Do not override existing files.
+        for idx, name in enumerate(self.names):
+            # Do not overwrite existing files.
             fname = f'data_{name}.json'
-            fpath = os.path.join(tmp_dir, fname)
+            fpath = os.path.join(self.tmp_dir, fname)
 
             if os.path.exists(fpath):
                 continue
@@ -92,7 +92,7 @@ class DnsTopCrawler(BaseCrawler):
             fpaths[name] = fpath
 
             batch.append(name)
-            if len(batch) < BATCH_SIZE and i < len(self.names):
+            if len(batch) < BATCH_SIZE and idx < len(self.names):
                 # Batch not yet full and not in last iteration.
                 continue
 
@@ -106,53 +106,78 @@ class DnsTopCrawler(BaseCrawler):
                 get_params += f'&dateRange=7d&domain={domain}'
 
             url = self.url + get_params
-            future = req_session.get(url)
-            future.domains = batch
-            future.fpaths = fpaths
-            queries.append(future)
+            self.batches[batch_id] = {'url': url, 'domains': batch, 'fpaths': fpaths}
             batch = list()
             fpaths = dict()
+            batch_id += 1
+        logging.info(f'Prepared {batch_id} batches')
 
-        for query in as_completed(queries):
-            try:
-                res = query.result()
-                res.raise_for_status()
-                # Confirm JSON integrity.
-                data = res.json()
-                if not data['success']:
-                    raise ValueError('Response contains success=False')
+    def fetch(self):
+        """Download data for top RANK_THRESHOLD domain names registered in IYP and save
+        it on disk."""
 
-                data = data['result']
+        self.__init_session()
 
-                if not self.reference['reference_time_modification']:
-                    # Get the reference time from the first file.
-                    try:
-                        date_str = data['meta']['dateRange'][0]['endTime']
-                        date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-                        self.reference['reference_time_modification'] = date
-                    except (KeyError, ValueError, TypeError) as e:
-                        logging.warning(f'Failed to get modification time: {e}')
+        num_retries = 0
+        while self.batches and num_retries <= MAX_RETRIES:
+            queries = list()
+            for batch_id, batch_data in self.batches.items():
+                future = self.session.get(batch_data['url'])
+                future.batch_id = batch_id
+                queries.append(future)
+            logging.info(f'Queued {len(self.batches)} requests. Try: {num_retries}')
+            for query in as_completed(queries):
+                try:
+                    res = query.result()
+                    if res.status_code == 429:
+                        logging.warning('Got HTTP 429 too many requests. Sleeping for 5 minutes.')
+                        self.executor.shutdown(wait=True, cancel_futures=True)
+                        self.session.close()
+                        sleep(5 * 60)
+                        self.__init_session()
+                        break
 
-                for idx, (placeholder, result) in enumerate(data.items()):
-                    if placeholder == 'meta':
-                        continue
-                    domain = query.domains[idx]
-                    with open(query.fpaths[domain], 'w') as fp:
-                        json.dump({domain: result}, fp)
+                    res.raise_for_status()
+                    # Confirm JSON integrity.
+                    data = res.json()
+                    if not data['success']:
+                        raise ValueError('Response contains success=False')
 
-            except Exception as e:
-                logging.error(f'Failed to fetch data for domains: {query.domains}: {e}')
-                continue
+                    data = data['result']
+
+                    if not self.reference['reference_time_modification']:
+                        # Get the reference time from the first file.
+                        try:
+                            date_str = data['meta']['dateRange'][0]['endTime']
+                            date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+                            self.reference['reference_time_modification'] = date
+                        except (KeyError, ValueError, TypeError) as e:
+                            logging.warning(f'Failed to get modification time: {e}')
+
+                    # Remove successful queries from batches list.
+                    batch_info = self.batches.pop(query.batch_id)
+                    for idx, (placeholder, result) in enumerate(data.items()):
+                        if placeholder == 'meta':
+                            continue
+                        domain = batch_info['domains'][idx]
+                        with open(batch_info['fpaths'][domain], 'w') as fp:
+                            json.dump({domain: result}, fp)
+
+                except Exception as e:
+                    logging.error(f'Failed to fetch data for domains: {self.batches[query.batch_id]["domains"]}: {e}')
+                    continue
+            num_retries += 1
+        if self.batches:
+            raise RuntimeError(f'Failed to fetch data for {len(self.batches)} batches.')
 
     def run(self):
+        self.make_batches()
         self.fetch()
 
-        tmp_dir = self.get_tmp_dir()
-
-        for entry in os.scandir(tmp_dir):
+        for entry in os.scandir(self.tmp_dir):
             if not entry.is_file() or not entry.name.endswith('.json'):
                 continue
-            file = os.path.join(tmp_dir, entry.name)
+            file = os.path.join(self.tmp_dir, entry.name)
 
             with open(file, 'rb') as fp:
                 results = json.load(fp)
