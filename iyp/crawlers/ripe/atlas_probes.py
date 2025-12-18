InternetHealthReport/internet-yellow-pages

commit ae671098793b26f4f0351df72affb0a07c6edb40

Fix ripe.atlas_probes crawler
There is one probe with invalid WGS 84 coordinates.
Valid longitude range is -180 to 180, but this probe has a longitude of
236, causing an overflow of the WGS84Point type, which breaks the
point_id map. Therefore, check if the coordinates are in range and if
not ignore them.

iyp/crawlers/ripe/atlas_probes.py
@@ -128,7 +128,12 @@ class Crawler(BaseCrawler):
             if probe['geometry']:
                 geo_coordinates = probe['geometry']['coordinates']
                 if geo_coordinates:
-                    points.add(WGS84Point((geo_coordinates[0], geo_coordinates[1])))
+                    long, lat = geo_coordinates
+                    # There is at least one probe with invalid coordinates.
+                    if long < -180 or long > 180 or lat < -90 or long > 90:
+                        probe['geometry'] = None
+                    else:
+                        points.add(WGS84Point((long, lat)))
 
             # An empty tag list causes a FlatterDict instance to remain in the object,
             # which neo4j does not like.

commit d9b9091209fe7203b5e122dd3297f81736876727

Fix ripe.atlas_probes crawler geo coordinate handling
Fix regression introduced in ae671098793b26f4f0351df72affb0a07c6edb40
that lead to ignoring the geo coordinates of all probes with a longitude
greater than 90 degrees.

iyp/crawlers/ripe/atlas_probes.py
@@ -130,8 +130,9 @@ class Crawler(BaseCrawler):
                 if geo_coordinates:
                     long, lat = geo_coordinates
                     # There is at least one probe with invalid coordinates.
-                    if long < -180 or long > 180 or lat < -90 or long > 90:
+                    if long < -180 or long > 180 or lat < -90 or lat > 90:
                         probe['geometry'] = None
+                        logging.warning(f'Ignoring invalid geo coordinates of probe {probe_id}: {lat=} {long=}')
                     else:
                         points.add(WGS84Point((long, lat)))
 