ccd2dcb93163fb975f9a4f9a11eeb9facd04bcd9
Aditi Singh <aditi.singh0205@gmail.com>
2026-01-13 15:23:56 +0530

    refactor(crawler): remove MANAGED_BY and LOCATED_IN relationships
    Per maintainer feedback:
    - Remove MANAGED_BY as Tag nodes already capture AWS context
    - Remove LOCATED_IN as region reflects data center location, not prefix geolocation
    - Keep only Prefix → CATEGORIZED → Tag relationship

--- iyp/crawlers/amazon/aws_ip_ranges.py
+++ iyp/crawlers/amazon/aws_ip_ranges.py
@@ -5,26 +5,8 @@ from datetime import datetime
 import requests
 from iyp import BaseCrawler
 
-ORG = 'Amazon Web Services'
 URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
 NAME = 'amazon.aws_ip_ranges'
-
-REGION_TO_COUNTRY = {
-    'us-east-1': 'US', 'us-east-2': 'US', 'us-west-1': 'US', 'us-west-2': 'US',
-    'af-south-1': 'ZA',
-    'ap-east-1': 'HK', 'ap-south-1': 'IN', 'ap-south-2': 'IN',
-    'ap-northeast-1': 'JP', 'ap-northeast-2': 'KR', 'ap-northeast-3': 'JP',
-    'ap-southeast-1': 'SG', 'ap-southeast-2': 'AU',
-    'ap-southeast-3': 'ID', 'ap-southeast-4': 'AU',
-    'ca-central-1': 'CA',
-    'eu-central-1': 'DE', 'eu-central-2': 'CH',
-    'eu-west-1': 'IE', 'eu-west-2': 'GB', 'eu-west-3': 'FR',
-    'eu-south-1': 'IT', 'eu-south-2': 'ES',
-    'eu-north-1': 'SE',
-    'me-south-1': 'BH', 'me-central-1': 'AE',
-    'sa-east-1': 'BR',
-    'us-gov-east-1': 'US', 'us-gov-west-1': 'US'
-}
 
 
 class Crawler(BaseCrawler):
@@ -53,9 +35,6 @@ class Crawler(BaseCrawler):
                 self.reference['reference_time_modification'] = dt
             except ValueError:
                 logging.warning(f"Could not parse createDate: {data['createDate']}")
-
-        # Create Organization
-        org_id = self.iyp.get_node('Organization', {'name': ORG}, ['name'])
 
         prefixes_v4 = []
         prefixes_v6 = []
@@ -91,41 +70,14 @@ class Crawler(BaseCrawler):
         all_prefix_ids = {**prefix_v4_ids, **prefix_v6_ids}
 
         # 2. Prepare Links
-        managed_by_links = []
-        located_in_links = []
         categorized_links = []
-
         tag_cache = {}
-        country_cache = {}
 
         for prefix, region, service, version in items:
             if prefix not in all_prefix_ids:
                 continue
 
             p_node_id = all_prefix_ids[prefix]
-
-            # MANAGED_BY Organization
-            managed_by_links.append({
-                'src_id': p_node_id,
-                'dst_id': org_id,
-                'props': [self.reference]
-            })
-
-            # LOCATED_IN Country
-            country_code = REGION_TO_COUNTRY.get(region)
-            if country_code:
-                if country_code not in country_cache:
-                    c_id = self.iyp.get_node(
-                        'Country', {'country_code': country_code}, ['country_code']
-                    )
-                    country_cache[country_code] = c_id
-
-                if country_cache[country_code]:
-                    located_in_links.append({
-                        'src_id': p_node_id,
-                        'dst_id': country_cache[country_code],
-                        'props': [self.reference]
-                    })
 
             # CATEGORIZED (Service Tag)
             if service:
@@ -140,17 +92,13 @@ class Crawler(BaseCrawler):
                 })
 
         # 3. Batch Create Links
-        if managed_by_links:
-            self.iyp.batch_add_links('MANAGED_BY', managed_by_links)
-        if located_in_links:
-            self.iyp.batch_add_links('LOCATED_IN', located_in_links)
         if categorized_links:
             self.iyp.batch_add_links('CATEGORIZED', categorized_links)
 
         logging.info(f"Refreshed AWS IP Ranges: {len(items)} prefixes processed.")
 
     def unit_test(self):
-        return super().unit_test(['MANAGED_BY', 'CATEGORIZED', 'LOCATED_IN'])
+        return super().unit_test(['CATEGORIZED'])
 
 
 def main():
@@ -168,7 +116,7 @@ def main():
 
     logging.info(f'Started: {sys.argv}')
 
-    crawler = Crawler(ORG, URL, NAME)
+    crawler = Crawler(URL, URL, NAME)
     if args.unit_test:
         crawler.unit_test()
     else:
