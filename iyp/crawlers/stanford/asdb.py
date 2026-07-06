bc52dab33750fae8bb8300671d14621ef7f5dfa0
Malte Tashiro <malte@iij.ad.jp>
2026-07-06 07:08:19 +0000

    Fix stanford.asdb crawler
    * Website got updated so paths changed.

--- iyp/crawlers/stanford/asdb.py
+++ iyp/crawlers/stanford/asdb.py
@@ -5,49 +5,42 @@ import re
 import sys
 from datetime import datetime, timezone
 
-import bs4
 import requests
-from bs4 import BeautifulSoup
 
 from iyp import BaseCrawler
 
-
-def get_latest_asdb_dataset_url(asdb_stanford_data_url: str, file_name_format: str):
-    response = requests.get(asdb_stanford_data_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    latest_date_element: bs4.element.Tag = soup.find('div', class_='col-md-12').find('p')
-    date_regex = re.compile(r'\d{1,2}/\d{1,2}/\d{4}')
-    date_string: str = date_regex.search(latest_date_element.text).group()
-    date: datetime = datetime.strptime(date_string, '%m/%d/%Y')
-    dateset_file_name: str = date.strftime(file_name_format)
-    asdb_stanford_data_url_formated: str = asdb_stanford_data_url.replace('#', '')
-    full_url: str = f'{asdb_stanford_data_url_formated}/{dateset_file_name}'
-    return full_url
-
-
-URL = get_latest_asdb_dataset_url('https://asdb.stanford.edu/#data', '%Y-%m_categorized_ases.csv')
+URL = 'https://asdb.stanford.edu/about'
 ORG = 'Stanford'
 NAME = 'stanford.asdb'
+DATA_URL_FMT = 'https://asdb.stanford.edu/static-website/data/%Y-%m_categorized_ases.csv'
 
 
 class Crawler(BaseCrawler):
     def __init__(self, organization, url, name):
         super().__init__(organization, url, name)
-        self.reference['reference_url_info'] = 'https://asdb.stanford.edu/'
-        self.__set_modification_time_from_url()
+        self.reference['reference_url_info'] = 'https://asdb.stanford.edu/about'
+        self.__get_latest_asdb_dataset_url()
 
-    def __set_modification_time_from_url(self):
-        fmt = 'https://asdb.stanford.edu/data/%Y-%m_categorized_ases.csv'
-        try:
-            date = datetime.strptime(URL, fmt).replace(tzinfo=timezone.utc)
-            self.reference['reference_time_modification'] = date
-        except ValueError as e:
-            logging.warning(f'Failed to set modification time: {e}')
+    def __get_latest_asdb_dataset_url(self):
+        date_regex = re.compile(r'Dataset was last updated and published on \d{1,2}/\d{1,2}/\d{4}')
+        response = requests.get(URL)
+        if response.status_code != 200:
+            logging.error(f'Failed to access landing page {URL}: {response.status_code}')
+            raise RuntimeError(f'Failed to access landing page {URL}: {response.status_code}')
+        date_match = date_regex.search(response.text)
+        if date_match is None:
+            logging.error('Failed to find last-published data in HTML source.')
+            raise RuntimeError('Failed to find last-published data in HTML source.')
+        date_string = date_match.group().split()[-1]
+        date = datetime.strptime(date_string, '%m/%d/%Y').replace(tzinfo=timezone.utc)
+        dataset_url = date.strftime(DATA_URL_FMT)
+        self.reference['reference_url_data'] = dataset_url
+        self.reference['reference_time_modification'] = date
 
     def run(self):
         """Fetch the ASdb file and push it to IYP."""
 
-        req = requests.get(URL)
+        req = requests.get(self.reference['reference_url_data'])
         req.raise_for_status()
 
         lines = set()
